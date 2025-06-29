a07df5f78014ff2e270568f064f2c3cb72a4144c
Jason Lin <jason.lin@remitbee.com>
2025-06-29 15:47:38 -0700

    added baseline boilerplate functions

---
+++ data_processing.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def load_data(path):
+    pass
+
+def clean_data(df):
+    pass
+
+def get_features_and_labels(df):
+    pass
+
+def split_data(X, y, test_size=0.2):
+    pass
---
+++ main.py
@@ -0,0 +1,7 @@
+from data_processing import load_data, clean_data, get_features_and_labels, split_data #change if adding/removing new functions
+from sklearn.neighbors import KNeighborsClassifier #change based on what model you use
+from sklearn.metrics import accuracy_score
+
+if __name__ == "__main__":
+    #use functions from data_processing here
+    pass
