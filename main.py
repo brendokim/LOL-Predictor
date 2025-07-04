d9e5b033f4190fe82e6fe77c0825eeb001c8e9f7
Kenneth <kennethsj77@gmail.com>
2025-07-04 13:48:01 -0700

    init commit

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from data_processing import load_data, clean_data, get_features_and_labels, split_data #change if adding/removing new functions
 from sklearn.neighbors import KNeighborsClassifier #change based on what model you use
 from sklearn.metrics import accuracy_score
+from data_processing import load_data, clean_data, get_features_and_labels, split_data, create_model, train_model
+from keras.models import Sequential
 
 if __name__ == "__main__":
-    #use functions from data_processing here
-    pass
+    def main():
+        df = load_data("match_data_v5.csv")
+        df = clean_data(df)
+        X, y = get_features_and_labels(df)
+        X_train, X_test, y_train, y_test = split_data(X, y)
+        X_train_scaled, X_test_scaled = scale_features(X_train, X_test)
+        model=create_model(X_train.shape[1])
+        train_model(model, X_train_scaled, y_train, X_test_scaled, y_test)
+        
+    main()
